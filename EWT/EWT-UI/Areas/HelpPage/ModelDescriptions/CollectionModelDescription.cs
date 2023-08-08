b03b07d88485a5bb556707c3a9693491bf67096a
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-09 01:09:05 +0600

    Init the Presentation Layer
    - Created the UI layer with generated VS code

---
+++ EWT/EWT-UI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace EWT_UI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ EWT/EWT-UI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EWT_UI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
