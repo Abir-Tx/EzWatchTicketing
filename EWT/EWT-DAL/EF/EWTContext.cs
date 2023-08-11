ae51c939c1087c338d3dfe8f601a72c6f1002866
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-12 02:26:30 +0600

    Create initial model and context
    - Created a basic user model
    - Created the basic EWTContext

---
+++ EWT/EWT-DAL/EF/EWTContext.cs
@@ -0,0 +1,10 @@
+﻿using EWT_DAL.EF.Models;
+using System.Data.Entity;
+
+namespace EWT_DAL.EF
+{
+    public class EWTContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ EWT/EWT-DAL/EF/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWT_DAL.EF.Models
+{
+    public class User
+    {
+        [Key] public int Id { get; set; }
+        [Required] public string Name { get; set; }
+        [Required] [StringLength(100)] public string Email { get; set; }
+        [Required] [StringLength(20)] public string Password { get; set; }
+    }
+}
