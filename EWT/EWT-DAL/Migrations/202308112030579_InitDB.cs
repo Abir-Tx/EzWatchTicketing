eef5b2a879286417b4d1b41aed26133d8e2f7288
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-12 02:37:35 +0600

    Enable and create init migration files

---
+++ EWT/EWT-DAL/Migrations/202308112030579_InitDB.cs
@@ -0,0 +1,28 @@
+﻿namespace EWT_DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitDB : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Email = c.String(nullable: false, maxLength: 100),
+                        Password = c.String(nullable: false, maxLength: 20),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
