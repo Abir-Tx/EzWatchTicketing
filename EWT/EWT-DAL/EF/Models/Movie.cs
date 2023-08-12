a72d6d5e37edbd18ab90c1fe623336f716fbbc87
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-12 22:40:26 +0600

    Init the Movie entity and create the CF db

---
+++ EWT/EWT-DAL/EF/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWT_DAL.EF.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [StringLength(100)]
+        public string Genre { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        public int Duration { get; set; }
+
+        [StringLength(1000)]
+        public string Description { get; set; }
+
+        [StringLength(255)]
+        public string Poster { get; set; }
+    }
+}
