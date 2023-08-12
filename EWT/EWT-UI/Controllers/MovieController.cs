b549f61cdd1d34dc963fb433eb53e046e9644c5e
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-12 23:51:34 +0600

    Implement the CRUD API routes

---
+++ EWT/EWT-UI/Controllers/MovieController.cs
@@ -0,0 +1,38 @@
+﻿using EWT_BLL.DTOs;
+using EWT_BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EWT_UI.Controllers
+{
+    public class MovieController : ApiController
+    {
+        [HttpGet]
+        [Route("api/movie/all")]
+        public HttpResponseMessage All()
+        {
+            try { return Request.CreateResponse(HttpStatusCode.OK, MovieService.Get()); }
+            catch (Exception ex) { return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message); }
+        }
+
+        [HttpGet]
+        [Route("api/movie/get/{id}")]
+        public HttpResponseMessage GetById(int id)
+        {
+            try { return Request.CreateResponse(HttpStatusCode.OK, MovieService.Get(id)); }
+            catch (Exception ex) { return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message); }
+        }
+
+        [HttpPost]
+        [Route("api/movie/create")]
+        public HttpResponseMessage Create(MovieDTO movie)
+        {
+            MovieService.Create(movie);
+            return Request.CreateResponse(HttpStatusCode.OK, "Movie id: " + movie.Id + " has been added to the database");
+        }
+    }
+}
