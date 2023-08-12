5f726931538b7b9cbbb12314324c312e8696469c
Masavi99 <masavi.aiub@gmail.com>
2023-08-13 02:35:55 +0600

    hall

---
+++ EWT/EWT-DAL/EF/Models/Hall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWT_DAL.EF.Models
+{
+  public class Hall
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        
+        
+    }
+}
