ac633a68dc4593cc5bdcf43c37dc28cf7f0e88aa
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-12 23:12:27 +0600

    Create Movie DTO class

---
+++ EWT/EWT-BLL/DTOs/MovieDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWT_BLL.DTOs
+{
+    public class MovieDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int Duration { get; set; }
+        public string Description { get; set; }
+        public string Poster { get; set; }
+    }
+}
