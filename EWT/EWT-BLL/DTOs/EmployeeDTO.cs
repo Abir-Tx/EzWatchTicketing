4360dff4bd2609b4412696e4412cc3a5a2736607
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-12 15:08:51 +0600

    Create Employee DTO
    - Added AutoMapper package
    - Created Emp DTO

---
+++ EWT/EWT-BLL/DTOs/EmployeeDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWT_BLL.DTOs
+{
+    public class EmployeeDTO
+    {
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Salary { get; set; }
+    }
+}
