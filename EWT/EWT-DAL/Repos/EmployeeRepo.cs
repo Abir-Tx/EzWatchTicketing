1aae0e6a7aaae634c43129a0224f44654a836adc
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-12 14:46:01 +0600

    Init Employee Repo

---
+++ EWT/EWT-DAL/Repos/EmployeeRepo.cs
@@ -0,0 +1,38 @@
+﻿using EWT_DAL.EF.Models;
+using EWT_DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWT_DAL.Repos
+{
+    internal class EmployeeRepo : Repo, IRepo<Employee, string, bool>
+    {
+        public bool Create(Employee obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Employee obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
