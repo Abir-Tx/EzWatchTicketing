961b6fd6c979645a64e8f0d46c3516c3b6ef1450
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-14 04:44:19 +0600

    Create the token model

---
+++ EWT/EWT-DAL/EF/Models/Token.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWT_DAL.EF.Models
+{
+    public class Token
+    {
+        public int Id { get; set; }
+        public string TokenKey { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ExpiredAt { get; set; }
+        [ForeignKey("User")]
+        public string Username { get; set; }
+        public virtual User User { get; set; }
+    }
+}
