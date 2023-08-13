be0ecf4d7973153ca35fc9b308b6b6725e3797ef
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-14 04:48:29 +0600

    Create initial token repo

---
+++ EWT/EWT-DAL/Repos/TokenRepo.cs
@@ -0,0 +1,40 @@
+﻿using EWT_DAL.EF.Models;
+using EWT_DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWT_DAL.Repos
+{
+    internal class TokenRepo : Repo, IRepo<Token, int, Token>
+    {
+        public Token Create(Token obj)
+        {
+            db.Tokens.Add(obj);
+            db.SaveChanges();
+            return obj;
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Token> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Token Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Token Update(Token obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
