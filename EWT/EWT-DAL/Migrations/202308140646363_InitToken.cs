0e473045909ad6da7294cb3012aa66d8a07d5c81
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-14 13:02:34 +0600

    Add token init migration

---
+++ EWT/EWT-DAL/Migrations/202308140646363_InitToken.cs
@@ -0,0 +1,28 @@
+﻿namespace EWT_DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitToken : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Tokens",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        TokenKey = c.String(),
+                        CreatedAt = c.DateTime(nullable: false),
+                        ExpiredAt = c.DateTime(),
+                        Username = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Tokens");
+        }
+    }
+}
