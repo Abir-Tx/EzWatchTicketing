54981a9a4de9103ea0549e08c89d2ba056c31b83
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-21 17:17:41 +0600

    Create a movie controller and render them in html

--- EWT/EWT-UI/Scripts/Angular/App.js
+++ EWT/EWT-UI/Scripts/Angular/App.js
@@ -1,5 +1,18 @@
-var app = angular.module("EWT", ["rzModule"]);
+var app = angular.module("EWT", []);
 
 app.controller("HomeController", function ($scope) {
     $scope.welcomeMessage = "Welcome to EasyWatchTicketing!";
-    });
+});
+
+app.controller("MovieController", function ($scope, $http) {
+    $scope.welcomeMessage = "Welcome to EasyWatchTicketing!";
+
+    $http.get("https://localhost:44365/api/movie/all")
+        .then(function (response) {
+            $scope.movies = response.data;
+            console.log("Data Fetching Successfull");
+        })
+        .catch(function (error) {
+            console.error("Error fetching data:", error);
+        });
+});
