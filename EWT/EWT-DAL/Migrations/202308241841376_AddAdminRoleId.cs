113868e99aa679cace8b0f1cf80d9f86b3edb12a
Neloy <20-42996-1@student.aiub.edu>
2023-08-25 01:06:17 +0600

    Admin All Updated

---
+++ EWT/EWT-DAL/Migrations/202308241841376_AddAdminRoleId.cs
@@ -0,0 +1,22 @@
+﻿namespace EWT_DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAdminRoleId : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Admins", "RoleId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Admins", "RoleId");
+            AddForeignKey("dbo.Admins", "RoleId", "dbo.Roles", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Admins", "RoleId", "dbo.Roles");
+            DropIndex("dbo.Admins", new[] { "RoleId" });
+            DropColumn("dbo.Admins", "RoleId");
+        }
+    }
+}
