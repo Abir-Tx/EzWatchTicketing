0214a370963f06c4d2359204054b6a381cd5e693
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2023-08-28 14:01:20 +0600

    Add another migration to diminish erros

---
+++ EWT/EWT-DAL/Migrations/202308280725470_Polish.cs
@@ -0,0 +1,24 @@
+﻿namespace EWT_DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Polish : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "RoleId", c => c.Int(nullable: false));
+            AddColumn("dbo.Users", "Username", c => c.String());
+            CreateIndex("dbo.Users", "RoleId");
+            AddForeignKey("dbo.Users", "RoleId", "dbo.Roles", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Users", "RoleId", "dbo.Roles");
+            DropIndex("dbo.Users", new[] { "RoleId" });
+            DropColumn("dbo.Users", "Username");
+            DropColumn("dbo.Users", "RoleId");
+        }
+    }
+}
