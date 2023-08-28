f2fec70deb7d535bafa77916051e574f5e47c8bf
Masavi99 <masavi.aiub@gmail.com>
2023-08-29 04:54:22 +0600

    dtos

--- EWT/EWT-BLL/DTOs/HallDTO.cs
+++ EWT/EWT-BLL/DTOs/HallDTO.cs
@@ -12,8 +12,8 @@ namespace EWT_BLL.DTOs
         public int Id { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
+        public int TotalSeats { get; set; }
 
-      
 
 
 
---
+++ EWT/EWT-BLL/DTOs/OrderFoodDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWT_BLL.DTOs
+{
+    public class OrderFoodDTO
+    {
+
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+    }
+}
