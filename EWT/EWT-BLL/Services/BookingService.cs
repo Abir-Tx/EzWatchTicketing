604c01053e39ac4e0f9f567732ca7d0511aff03d
Masavi99 <masavi.aiub@gmail.com>
2023-08-29 06:39:03 +0600

    service_updated

---
+++ EWT/EWT-BLL/Services/BookingService.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using EWT_BLL.DTOs;
+using EWT_DAL.EF.Models;
+using EWT_DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace EWT_BLL.Services
+{
+    public class BookingService
+    {
+        public static List<BookingDTO> Get()
+        {
+            var data = DataAccesser.BookingDataAccess().Get();
+            var config = new MapperConfiguration(cfg =>
+            cfg.CreateMap<Booking , BookingDTO>());
+            var mapper = new Mapper(config);
+            var convertData = mapper.Map<List<BookingDTO>>(data);
+            return convertData;
+
+        }
+        //byid
+        public static BookingDTO Get(int id)
+        {
+            var data = DataAccesser.BookingDataAccess().Get(id);
+            var config = new MapperConfiguration(cfg =>
+            cfg.CreateMap<Booking, BookingDTO>());
+            var mapper = new Mapper(config);
+            var convertData = mapper.Map<BookingDTO>(data);
+            return convertData;
+        }
+        //getbyuserid
+        //createbooking
+
+        public static bool create(BookingDTO booking)
+        {
+            if (booking == null)
+                return false;
+            else
+            {
+
+                var config = new MapperConfiguration(cfg =>
+                cfg.CreateMap<BookingDTO, Booking>());
+                var mapper = new Mapper(config);
+                var convertData = mapper.Map<Booking>(booking);
+                return DataAccesser.BookingDataAccess().Create(convertData);
+
+            }
+
+
+        }
+        //update
+        public static bool update(BookingDTO booking)
+        {
+            if (booking == null)
+                return false;
+            else
+            {
+
+                var config = new MapperConfiguration(cfg =>
+                cfg.CreateMap<BookingDTO, Booking>());
+                var mapper = new Mapper(config);
+                var convertData = mapper.Map<Booking>(booking);
+                return DataAccesser.BookingDataAccess().Update(convertData);
+
+            }
+
+        }
+        //delete
+        public static bool Delete(int id)
+        {
+            return DataAccesser.BookingDataAccess().Delete(id);   
+        }
+
+
+    }
+
+
+
+}
--- EWT/EWT-BLL/Services/FoodService.cs
+++ EWT/EWT-BLL/Services/FoodService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using EWT_BLL.DTOs;
+using EWT_DAL;
+using EWT_DAL.EF.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +12,62 @@ namespace EWT_BLL.Services
 {
     public class FoodService
     {
+        public static List<FoodDTO> Get()
+        {
+            var data= DataAccesser.FoodDataAccess().Get();
+            var config = new MapperConfiguration(cfg =>
+            cfg.CreateMap<Food, FoodDTO>());
+            var mapper = new Mapper(config);
+            var convertData=mapper.Map<List<FoodDTO>>(data);    
+            return convertData; 
+        }
+        //byid
+        public static FoodDTO Get(int id)
+        {
+            var data = DataAccesser.FoodDataAccess().Get(id);
+            var config = new MapperConfiguration(cfg =>
+            cfg.CreateMap<Food, FoodDTO>());
+            var mapper = new Mapper(config);
+            var convertData = mapper.Map<FoodDTO>(data);
+            return convertData;
+        }
+        //create
+        public static bool create(FoodDTO food)
+        {
+            if (food==null)
+                return false;
+            else
+            {
+                var config = new MapperConfiguration(cfg =>
+                cfg.CreateMap<FoodDTO, Food>());
+                var mapper = new Mapper(config);
+                var convertData = mapper.Map<Food>(food);
+                return DataAccesser.FoodDataAccess().Create(convertData);
+
+            }
+        }
+        //delete
+        public static bool Delete(int id)
+        {
+            return DataAccesser.FoodDataAccess().Delete(id);    
+        }
+        //update
+        public static bool update(FoodDTO food)
+        {
+            if (food == null)
+                return false;
+            else
+            {
+                var config = new MapperConfiguration(cfg =>
+                cfg.CreateMap<FoodDTO, Food>());
+                var mapper = new Mapper(config);
+                var convertData = mapper.Map<Food>(food);
+
+                return DataAccesser.FoodDataAccess().Update(convertData);
+
+            }
+
+        }
+
     }
 }
